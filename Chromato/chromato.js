745816c415cac7b0ba1735c7fb8118e914b48143
ObiwanBox <connorknabe@gmail.com>
2014-11-06 10:08:28 -0500

    now takes in the apikey

--- Chromato/chromato.js
+++ Chromato/chromato.js
@@ -2,28 +2,32 @@ var express = require('express'); var bodyParser =
 require('body-parser'); 
 
 var app = express.createServer();
-//app.use(bodyParser()); 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(bodyParser()); 
+//app.use(bodyParser.urlencoded());
+//app.use(bodyParser.json());
 var http = require('http');
 var url = require('url');
 var request = require('request');
 
 app.get('/', function(req, res){
-  var html = '<form action="/" method="post">' +
+    var html = '<form action="/" method="post">' +
                'Enter your name:' +
-               '<input type="text" name="userName" placeholder="..." />' 
-+
+               '<input type="text" name="movieTitle" placeholder="..." />' 
++ 			   '<input type="text" name="apiKey" placeholder="apikey" />'
++			   
                '<br>' +
                '<button type="submit">Submit</button>' +
             '</form>';
                
-  res.send(html);
+    res.send(html);
 });
 app.post('/', function(req, res){
-  var userName = req.body.userName;
-  var html = 'Hello: ' + userName + '.<br>' +
+    var movieTitle = req.body.userName;
+	var 
+
+
+    var html = 'Hello: ' + userName + '.<br>' +
              '<a href="/">Try again.</a>';
-  res.send(html);
+    res.send(html);
 });
 app.listen(80);
